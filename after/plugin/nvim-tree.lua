7fe4be33b8801d8d5f02190f0e78cff5708d8867
Jayanth-Parthsarathy <jparthaa@gmail.com>
2023-04-29 20:19:10 +0530

    Add bufferline and gitsigns

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -4,19 +4,17 @@ vim.g.loaded_netrwPlugin = 1
 
 -- set termguicolors to enable highlight groups
 
-vim.opt.termguicolors = true
-
 -- empty setup using defaults
 require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
-    sort_by = "case_sensitive",
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = true,
-    },
+	sort_by = "case_sensitive",
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
-vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
--- lua/jayanth/plugins/nvim-tree.lua
+++ lua/jayanth/plugins/nvim-tree.lua
@@ -1,10 +1,11 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
+	"nvim-tree/nvim-web-devicons",
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({})
+	end,
 }
