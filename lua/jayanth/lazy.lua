d5fa77b27bef7b5ccaf1ff699299361380e86e23
Jayanth-Parthsarathy <jparthaa@gmail.com>
2023-09-28 13:28:11 +0530

    Remove the dev field from lazy.lua 🩹

--- lua/jayanth/lazy.lua
+++ lua/jayanth/lazy.lua
@@ -1,28 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "jayanth.plugins" }, { import = "jayanth.plugins.lsp" } }, {
-	install = {
-		colorscheme = { "ayu" },
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
-	dev = {
-		path = "~/Developments/Personal/gruvbin/lua/lush_theme/gruvbin.nvim",
-	},
+  install = {
+    colorscheme = { "ayu" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 })
